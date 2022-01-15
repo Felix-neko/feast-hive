7fcd13f86de6c126280f936e083d7ad0ed4e364b
karamel <felix-neko@list.ru>
2022-01-15 06:11:24 +0300

    Added in Impala template with deduplication...

--- feast_hive/hive.py
+++ feast_hive/hive.py
@@ -36,7 +36,7 @@ except ImportError as e:
 
 # default Hive settings that can be overridden in offline_store:hive_conf of feature_store.yaml
 DEFAULT_HIVE_CONF = {
-    "hive.strict.checks.cartesian.product": "false",
+    # "hive.strict.checks.cartesian.product": "false",
 }
 
 
@@ -165,7 +165,7 @@ class HiveOfflineStore(OfflineStore):
         end_date = _format_datetime(end_date)
 
         queries = [
-            "SET hive.resultset.use.unique.column.names=false",
+            # "SET hive.resultset.use.unique.column.names=false",
             f"""
             SELECT 
                 {field_string}
@@ -241,10 +241,10 @@ class HiveOfflineStore(OfflineStore):
                 # In order to use `REGEX Column Specification`, need set `hive.support.quoted.identifiers` to None.
                 # Can study more here: https://cwiki.apache.org/confluence/display/Hive/LanguageManual+Select
                 queries = [
-                    "SET hive.mapred.mode=nonstrict",
-                    "SET hive.support.quoted.identifiers=none",
-                    "SET hive.resultset.use.unique.column.names=false",
-                    "SET hive.exec.temporary.table.storage=memory",
+                    # "SET hive.mapred.mode=nonstrict",
+                    # "SET hive.support.quoted.identifiers=none",
+                    # "SET hive.resultset.use.unique.column.names=false",
+                    # "SET hive.exec.temporary.table.storage=memory",
                 ] + rendered_query.split(";")
 
                 yield queries
@@ -376,7 +376,7 @@ def _upload_entity_df_and_get_entity_schema(
         limited_entity_df = HiveRetrievalJob(
             conn,
             [
-                "SET hive.resultset.use.unique.column.names=false",
+                # "SET hive.resultset.use.unique.column.names=false",
                 f"SELECT * FROM {table_name} LIMIT 1",
             ],
         ).to_df()
@@ -519,190 +519,110 @@ def _get_entity_df_event_timestamp_range(
 # In case there was any improvement on BigQuery, probably need to apply here too.
 
 MULTIPLE_FEATURE_VIEW_POINT_IN_TIME_JOIN = """
-/*
- Compute a deterministic hash for the `left_table_query_string` that will be used throughout
- all the logic as the field to GROUP BY the data
-*/
-CREATE TEMPORARY TABLE entity_dataframe AS (
-    SELECT *,
-        {{entity_df_event_timestamp_col}} AS entity_timestamp
-        {% for featureview in featureviews %}
-            ,CONCAT(
-                {% for entity in featureview.entities %}
-                    CAST({{entity}} AS STRING),
-                {% endfor %}
-                CAST({{entity_df_event_timestamp_col}} AS STRING)
-            ) AS {{featureview.name}}__entity_row_unique_id
-        {% endfor %}
+{#
+entity_dataframe: для каждого извлекаемого entity мы прописываем следующие поля:
+- его составной ключ (полный entity id)
+- timestamp
+- и для каждой Feature View, которая входит в entity, прописываем специальный строковый ключ
+    {featureview}_entity_row_unique_id. Этот ключ собирается из строковых представлений:
+    - всех entity id, входящих в это FeatureView
+    - timestamp'а извлекаемого entity (не timestamp'а feature view, а именно извлекаемого entity)
+#}
+
+WITH entity_dataframe AS
+(
+    SELECT *, {{entity_df_event_timestamp_col}} AS entity_timestamp{% if featureviews|length > 0 %},{% endif %}
+        {%- for fv in featureviews %}
+            CONCAT(
+                {%- for entity in fv.entities %}CAST({{entity}} AS STRING), {% endfor -%}
+                CAST({{entity_df_event_timestamp_col}} AS STRING)) AS {{fv.name}}__entity_row_unique_id
+                {%- if not loop.last %},{% endif -%}
+        {%- endfor %}
     FROM {{ left_table_query_string }}
-);
+),
+{#
+Далее для каждого feature view мы создаём временную таблицу {feature_view}__outputs.
+В неё мы кладём те даные, которые нам удалось извлечь из этого feature view по заданным entity.
 
+Табличку {feature_view}__output формируем в несколько этапов.
 
--- Start create temporary table *__base
-{% for featureview in featureviews %}
+1) {feature_view}__entity_dataframe: кусочек таблицы entity_dataframe,
+но содержащий только row_unique_id только для этого feature view
 
-CREATE TEMPORARY TABLE {{ featureview.name }}__base AS
-WITH {{ featureview.name }}__entity_dataframe AS (
-    SELECT
-        {{ featureview.entities | join(', ')}},
-        entity_timestamp,
-        {{featureview.name}}__entity_row_unique_id
+2) {feature_view}__suitable_objects: ID объектов в feature view, подходящих под данное entity:
+- поля составного ключа entity key для данного feature view (NB: не полный, а именно для данного feature view)
+- entity_timestamp: временная метка из entity_dataframe: будем
+    извлекать объекты с датой не позднее, чем эта (плюс это поле будет участвовать в конечном JOINе)
+- max_event_timestamp: самый подздний event timestamp по всем найденным объектам в feature view, соответствующих
+    entity key, но не превышающих заданный в entity_timestamp (и не отстающих от него больше, чем на TTL)
+    Если такого объекта не найдено, то в таблице будет ещё на одну строку меньше = )
+
+3) {feature_view}__prefinal: а теперь добавляем в предыдущую табличку с ключами и timestamp'ами найденных объектов
+    ещё и собственно фичи из таблицы {feature}, от строк с заданными ключами и timestamp'ами.
+
+    Но: у нас могут быть дубликаты (т.е. несколько строк с одинаковыми entity key и event timestamp'ами),
+    для таких у нас будет row__duplicate__index > 1, записанный с помощью оконной функции ROW_NUMBER
+
+4) {feature_view}__output: тут дубликаты мы уже зачистим
+#}
+
+{%- for fv in featureviews %}
+{{ fv.name }}__entity_dataframe AS
+(
+    SELECT {{ fv.entities | join(', ')}}, entity_timestamp, {{fv.name}}__entity_row_unique_id
     FROM entity_dataframe
-    GROUP BY {{ featureview.entities | join(', ')}}, entity_timestamp, {{featureview.name}}__entity_row_unique_id
+    GROUP BY {{ fv.entities | join(', ')}}, entity_timestamp, {{fv.name}}__entity_row_unique_id
 ),
-
-/*
- This query template performs the point-in-time correctness join for a single feature set table
- to the provided entity table.
-
- 1. We first join the current feature_view to the entity dataframe that has been passed.
- This JOIN has the following logic:
-    - For each row of the entity dataframe, only keep the rows where the `event_timestamp_column`
-    is less than the one provided in the entity dataframe
-    - If there a TTL for the current feature_view, also keep the rows where the `event_timestamp_column`
-    is higher the the one provided minus the TTL
-    - For each row, Join on the entity key and retrieve the `entity_row_unique_id` that has been
-    computed previously
-
- The output of this CTE will contain all the necessary information and already filtered out most
- of the data that is not relevant.
-*/
-
-{{ featureview.name }}__subquery AS (
+{{ fv.name }}__suitable_objects AS
+(
+    SELECT {{ fv.name }}__entity_dataframe.{{ fv.name }}__entity_row_unique_id,
+        {% for entity in fv.entities %}{{ fv.name }}.{{ entity }},{% endfor %} MAX({{ fv.name }}.{{ fv.event_timestamp_column }}) AS max_event_timestamp
+    FROM {{ fv.name }}__entity_dataframe INNER JOIN {{ fv.name }} ON
+        {%- for entity in fv.entities %}
+            {% if not loop.first %}AND {% endif %}{{ fv.name }}__entity_dataframe.{{ entity }} = {{ fv.name }}.{{ entity }}
+        {% endfor -%}
+    WHERE {{ fv.name }}.{{ fv.event_timestamp_column }} <= {{ fv.name }}__entity_dataframe.entity_timestamp
+        AND {{ fv.name }}.{{ fv.event_timestamp_column }} >= {{ fv.name }}__entity_dataframe.entity_timestamp - interval {{ fv.ttl }} second
+    GROUP BY {% for entity in fv.entities %}{{ fv.name }}.{{ entity }}, {% endfor %}{{ fv.name }}__entity_dataframe.{{ fv.name }}__entity_row_unique_id
+),
+{{ fv.name }}__prefinal AS
+(
     SELECT
-        {{ featureview.event_timestamp_column }} as event_timestamp,
-        {{ featureview.created_timestamp_column ~ ' as created_timestamp,' if featureview.created_timestamp_column else '' }}
-        {{ featureview.entity_selections | join(', ')}}{% if featureview.entity_selections %},{% else %}{% endif %}
-        {% for feature in featureview.features %}
-            {{ feature }} as {% if full_feature_names %}{{ featureview.name }}__{{feature}}{% else %}{{ feature }}{% endif %}{% if loop.last %}{% else %}, {% endif %}
-        {% endfor %}
-    FROM {{ featureview.table_subquery }} AS subquery
-    INNER JOIN (
-        SELECT MAX(entity_timestamp) as max_entity_timestamp_
-               {% if featureview.ttl == 0 %}{% else %}
-               ,(MIN(entity_timestamp) - interval '{{ featureview.ttl }}' second) as min_entity_timestamp_
-               {% endif %}
-        FROM entity_dataframe
-    ) AS temp
-    ON (
-        {{ featureview.event_timestamp_column }} <= max_entity_timestamp_
-        {% if featureview.ttl == 0 %}{% else %}
-        AND {{ featureview.event_timestamp_column }} >=  min_entity_timestamp_
-        {% endif %}
-    )
-)
+        {{ fv.name }}__suitable_objects.{{ fv.name }}__entity_row_unique_id,
+        {%- for feature in fv.features %}
+        {{ fv.name }}.{{ feature }} AS {{ feature }},
+        {%- endfor %}
+        ROW_NUMBER() OVER(PARTITION BY {{ fv.name }}__suitable_objects.{{ fv.name }}__entity_row_unique_id
+            ORDER BY {{ fv.name }}.event_timestamp) AS {{ fv.name }}__duplicate_index
+    FROM {{ fv.name }}__suitable_objects INNER JOIN {{ fv.name }}
+    ON
+        {%- for entity in fv.entities %}
+        {% if not loop.first %}AND {% endif %}{{ fv.name }}__suitable_objects.{{ entity }} = {{ fv.name }}.{{ entity }}
+        {% endfor -%}
+        AND {{ fv.name }}__suitable_objects.max_event_timestamp = {{ fv.name }}.{{ fv.event_timestamp_column }}
+),
+{{ fv.name }}__output AS
+(
+    SELECT * FROM {{ fv.name }}__prefinal WHERE {{ fv.name }}__prefinal.{{ fv.name }}__duplicate_index = 1
+){% if not loop.last %},{% endif %}
+{%- endfor %}
+
+{#
+А теперь делаем жирный финальный JOIN: на строки entity_dataframe пытаемся навесить данные, найденные по ним
+    в featureview'шках (если ничего подходящего не нашлось, навешиваем None, NaN или NaT)
+#}
 SELECT
-    subquery.*,
-    entity_dataframe.entity_timestamp,
-    entity_dataframe.{{featureview.name}}__entity_row_unique_id
-FROM {{ featureview.name }}__subquery AS subquery
-INNER JOIN (
-    SELECT *
-    {% if featureview.ttl == 0 %}{% else %}
-    , (entity_timestamp - interval '{{ featureview.ttl }}' second) as ttl_entity_timestamp
-    {% endif %}
-    FROM {{ featureview.name }}__entity_dataframe
-) AS entity_dataframe
-ON ( 
-    subquery.event_timestamp <= entity_dataframe.entity_timestamp
-
-    {% if featureview.ttl == 0 %}{% else %}
-    AND subquery.event_timestamp >= entity_dataframe.ttl_entity_timestamp
-    {% endif %}
-
-    {% for entity in featureview.entities %}
-    AND subquery.{{ entity }} = entity_dataframe.{{ entity }}
-    {% endfor %}
-);
-
-{% endfor %}
--- End create temporary table *__base
-
-
-{% for featureview in featureviews %}
-
-{% if loop.first %}WITH{% endif %}
-
-/*
- 2. If the `created_timestamp_column` has been set, we need to
- deduplicate the data first. This is done by calculating the
- `MAX(created_at_timestamp)` for each event_timestamp.
- We then join the data on the next CTE
-*/
-{% if featureview.created_timestamp_column %}
-{{ featureview.name }}__dedup AS (
-    SELECT
-        {{featureview.name}}__entity_row_unique_id,
-        event_timestamp,
-        MAX(created_timestamp) as created_timestamp
-    FROM {{ featureview.name }}__base
-    GROUP BY {{featureview.name}}__entity_row_unique_id, event_timestamp
-),
-{% endif %}
-
-/*
- 3. The data has been filtered during the first CTE "*__base"
- Thus we only need to compute the latest timestamp of each feature.
-*/
-{{ featureview.name }}__latest AS (
-    SELECT
-        base.{{featureview.name}}__entity_row_unique_id,
-        MAX(base.event_timestamp) AS event_timestamp
-        {% if featureview.created_timestamp_column %}
-            ,MAX(base.created_timestamp) AS created_timestamp
-        {% endif %}
-
-    FROM {{ featureview.name }}__base AS base
-    {% if featureview.created_timestamp_column %}
-        INNER JOIN {{ featureview.name }}__dedup AS dedup
-        ON (
-            dedup.{{featureview.name}}__entity_row_unique_id=base.{{featureview.name}}__entity_row_unique_id
-            AND dedup.event_timestamp=base.event_timestamp
-            AND dedup.created_timestamp=base.created_timestamp
-        )
-    {% endif %}
-
-    GROUP BY base.{{featureview.name}}__entity_row_unique_id
-),
-
-/*
- 4. Once we know the latest value of each feature for a given timestamp,
- we can join again the data back to the original "base" dataset
-*/
-{{ featureview.name }}__cleaned AS (
-    SELECT base.*
-    FROM {{ featureview.name }}__base AS base
-    INNER JOIN {{ featureview.name }}__latest AS latest
-    ON (
-        base.{{featureview.name}}__entity_row_unique_id=latest.{{featureview.name}}__entity_row_unique_id
-        AND base.event_timestamp=latest.event_timestamp
-        {% if featureview.created_timestamp_column %}
-            AND base.created_timestamp=latest.created_timestamp
-        {% endif %}
-    )
-){% if loop.last %}{% else %}, {% endif %}
-
-
-{% endfor %}
-
-/*
- Joins the outputs of multiple time travel joins to a single table.
- The entity_dataframe dataset being our source of truth here.
- */
-
-SELECT `(entity_timestamp|{% for featureview in featureviews %}{{featureview.name}}__entity_row_unique_id{% if loop.last %}{% else %}|{% endif %}{% endfor %})?+.+`
+{%- for fv in featureviews -%}
+    {%- set outer_loop = loop -%}
+    {%- for feature in fv.features %}
+    {{ fv.name }}__output.{{ feature }} AS {% if full_feature_names %}{{ fv.name }}__{% endif -%}{{ feature }}
+    {%- if not (loop.last and outer_loop.last) %},{% endif %}
+    {%- endfor -%}
+{%- endfor %}
 FROM entity_dataframe
-{% for featureview in featureviews %}
-LEFT JOIN (
-    SELECT
-        {{featureview.name}}__entity_row_unique_id
-        {% for feature in featureview.features %}
-            ,{% if full_feature_names %}{{ featureview.name }}__{{feature}}{% else %}{{ feature }}{% endif %}
-        {% endfor %}
-    FROM {{ featureview.name }}__cleaned
-) AS {{ featureview.name }}__joined 
-ON (
-    {{ featureview.name }}__joined.{{featureview.name}}__entity_row_unique_id=entity_dataframe.{{featureview.name}}__entity_row_unique_id
-)
-{% endfor %}
+{%- for fv in featureviews %}
+    LEFT OUTER JOIN {{ fv.name }}__output
+        ON entity_dataframe.{{ fv.name }}__entity_row_unique_id = {{ fv.name }}__output.{{ fv.name }}__entity_row_unique_id
+    {% endfor %}
+
 """
